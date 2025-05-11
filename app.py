42b6485fca1e8c1e07fdb2a211e1ac278fc56d3b
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-11 13:29:36 -0300

    Se hacen ajustes app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import filtrar_dataset
 from flask import Flask, request, jsonify, render_template
 import subprocess
 import json
@@ -6,6 +7,8 @@ import logging
 import pandas as pd
 
 app = Flask(__name__)
+desde = "202401"
+hasta = "202412"
 
 # Configuración de Logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -15,16 +18,11 @@ KMEANS_SCRIPT = os.path.abspath("K-Means-Rotacion.py")
 # Añadimos la ruta al script de filtrado
 FILTRAR_SCRIPT = os.path.abspath("filtrar_dataset.py") #nuevo
 
-def ejecutar_filtrado(): #nuevo
+def ejecutar_filtrado(desde, hasta): #nuevo
     """Ejecuta el script de filtrado sin necesidad de variables externas."""
     try:
         # Ejecutamos el script de filtrado
-        resultado = subprocess.run(
-            ['python', FILTRAR_SCRIPT, "202401", "202412"],  # Ejecutamos con parámetros
-            capture_output=True, 
-            text=True, 
-            check=True
-        )
+        filtrar_dataset(desde, hasta)    
         logging.info("✅ Filtrado ejecutado correctamente.")
         return True
 
