2aae62ab0f27419705f0d8d04a533baa7f3d3c83
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-16 23:51:43 -0300

    Se agregan archivos serviceAccountKey.json y config_firebase.py

---
+++ config_firebase.py
@@ -0,0 +1,12 @@
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def init_firebase():
+    ruta_actual = os.path.dirname(os.path.abspath(__file__))
+    ruta_clave = os.path.join(ruta_actual, "serviceAccountKey.json")
+    cred = credentials.Certificate(ruta_clave)
+    firebase_admin.initialize_app(cred)
+    return firestore.client()
+
+db = init_firebase()
