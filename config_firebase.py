d21532b229caacad5c5755f68012a01246fc9493
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-17 12:01:49 -0300

    Se agrega print a config_firebase

--- config_firebase.py
+++ config_firebase.py
@@ -5,6 +5,7 @@ from firebase_admin import credentials, firestore
 def init_firebase():
     # Ruta relativa o absoluta al archivo
     cred_path = os.path.join(os.path.dirname(__file__), 'firebase-service.json')
+    print("Ruta a credenciales:", cred_path) 
     
     if not os.path.exists(cred_path):
         raise FileNotFoundError(f"No se encontró el archivo de credenciales en: {cred_path}")
