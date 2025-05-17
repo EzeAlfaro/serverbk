b8122bd23a5bca34bd9bb622f72d60dec3724516
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-17 00:30:39 -0300

    Se modifica config_fireabse.py

--- config_firebase.py
+++ config_firebase.py
@@ -1,15 +1,15 @@
 import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 def init_firebase():
-    firebase_json = os.environ.get("FIREBASE_KEY")
-    if not firebase_json:
-        raise ValueError("La variable de entorno FIREBASE_KEY no está definida")
-    # Convertimos el string JSON a dict
-    cred_dict = json.loads(firebase_json)
-    cred = credentials.Certificate(cred_dict)
+    # Ruta relativa o absoluta al archivo
+    cred_path = os.path.join(os.path.dirname(__file__), 'firebase_key.json')
+    
+    if not os.path.exists(cred_path):
+        raise FileNotFoundError(f"No se encontró el archivo de credenciales en: {cred_path}")
+    
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
     return firestore.client()
 
