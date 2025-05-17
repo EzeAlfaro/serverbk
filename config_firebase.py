d8c680912bfe33c86bfd0393ef85c43a7101926d
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-17 19:09:52 -0300

    Se modifica config_firebase

--- config_firebase.py
+++ config_firebase.py
@@ -1,20 +1,29 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+from cryptography.fernet import Fernet
 
 def init_firebase():
-    # Ruta relativa o absoluta al archivo
-    cred_path = os.path.join(os.path.dirname(__file__), 'firebase-service.json')
-    print("Ruta a credenciales:", cred_path) 
-    
-    if not os.path.exists(cred_path):
-        raise FileNotFoundError(f"No se encontró el archivo de credenciales en: {cred_path}")
-    
-    # Solo inicializa si no hay una app ya creada
+    clave_ferret = os.environ.get('KEY_FERRET')
+    if not clave_ferret:
+        raise ValueError("No se encontró la variable de entorno KEY_FERRET")
+
+    fernet = Fernet(clave_ferret.encode())
+
+    enc_path = os.path.join(os.path.dirname(__file__), 'service-firebase.enc')
+    if not os.path.exists(enc_path):
+        raise FileNotFoundError(f"No se encontró el archivo encriptado en: {enc_path}")
+
+    with open(enc_path, 'rb') as f:
+        data_encriptada = f.read()
+
+    data_json = fernet.decrypt(data_encriptada)
+
     if not firebase_admin._apps:
-        cred = credentials.Certificate(cred_path)
+        cred = credentials.Certificate(data_json)
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
 
 db = init_firebase()
+
