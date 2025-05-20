49e64879903a1fbe3dc1119164473819203ee3de
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-20 16:31:27 -0300

    se modifica config-firebase

--- config_firebase.py
+++ config_firebase.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 
 def init_firebase():
     ruta_actual = os.path.dirname(os.path.abspath(__file__))
-    ruta_clave = os.path.join(ruta_actual, "serviceAccountKey.json")
+    ruta_clave = os.path.join(ruta_actual, "firebase-service.json")
     cred = credentials.Certificate(ruta_clave)
     firebase_admin.initialize_app(cred)
     return firestore.client()
