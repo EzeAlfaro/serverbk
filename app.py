6926c35723769bd762f7e8b27091d53caa5bdf78
Cecilia Torales <ceciliactorales@gmail.com>
2025-05-20 23:55:53 -0300

    se refactoriza k-means.Rotacion.py como modulo, app.py lo importa, se modifica endpoint kmeans

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import subprocess
+from K_Means_Rotacion import ejecutar_kmeans
 import json
 import os
 import logging
@@ -22,24 +22,7 @@ KMEANS_SCRIPT = os.path.abspath("K-Means-Rotacion.py")
 
 
     
-def ejecutar_kmeans():
-    """Ejecuta el script K-Means y captura la salida."""
-    try:
-        # Ejecuta el script y captura la salida
-        resultado = subprocess.run(['python', KMEANS_SCRIPT], 
-                                 capture_output=True, 
-                                 text=True, 
-                                 check=True)
-        # Carga la salida JSON directamente
-        json_resultado = json.loads(resultado.stdout)
-        return json_resultado
 
-    except subprocess.CalledProcessError as e:
-        logging.error(f"Error al ejecutar K-Means: {e.stderr}")
-        raise Exception(f"Error al ejecutar K-Means: {e.stderr}")
-    except json.JSONDecodeError as e:
-        logging.error(f"Error al decodificar JSON: {e}")
-        raise Exception(f"Error al decodificar JSON: {e}")
 
 @app.route('/', methods=['GET'])
 def index():
@@ -66,12 +49,9 @@ def kmeans_endpoint():
 
     try:
         resultado_kmeans = ejecutar_kmeans()
-        # resultado_kmeans ya tiene la estructura {"data": [...]}
-        # Por eso acá devolvemos directo el resultado sin envolverlo de nuevo
         return jsonify(resultado_kmeans), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 
 
 @app.route('/guardar_resultados', methods=['POST'])
